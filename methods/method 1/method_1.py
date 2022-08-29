8dc0c6a896155ef7a18bdec2da0422d32032e0b7
svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2022-08-29 14:31:14 +0200

    Add missing import and function

--- methods/method 1/method_1.py
+++ methods/method 1/method_1.py
@@ -2,9 +2,9 @@
 # By svenskithesource (https://github.com/Svenskithesource)
 # Made for (https://github.com/Svenskithesource/PyArmor-Unpacker)
 
-import opcode, types, marshal
+import opcode, types, marshal, sys
 
-RETURN_OPCODE = opcode.opmap["RETURN_VALUE"].to_bytes(2, byteorder='little') # Convert to bytes so it can be added to bytes easier later on
+RETURN_OPCODE = opcode.opmap["RETURN_VALUE"].to_bytes(2, byteorder='little')  # Convert to bytes so it can be added to bytes easier later on
 SETUP_FINALLY = opcode.opmap["SETUP_FINALLY"]
 EXTENDED_ARG = opcode.opmap["EXTENDED_ARG"]
 LOAD_GLOBAL = opcode.opmap["LOAD_GLOBAL"]
@@ -17,9 +17,11 @@ POP_JUMP_IF_TRUE = opcode.opmap.get("POP_JUMP_IF_TRUE")
 JUMP_IF_FALSE_OR_POP = opcode.opmap.get("JUMP_IF_FALSE_OR_POP")
 JUMP_IF_TRUE_OR_POP = opcode.opmap.get("JUMP_IF_TRUE_OR_POP")
 
-absolute_jumps = [JUMP_ABSOLUTE, CONTINUE_LOOP, POP_JUMP_IF_FALSE, POP_JUMP_IF_TRUE, JUMP_IF_FALSE_OR_POP, JUMP_IF_TRUE_OR_POP]
+absolute_jumps = [JUMP_ABSOLUTE, CONTINUE_LOOP, POP_JUMP_IF_FALSE, POP_JUMP_IF_TRUE, JUMP_IF_FALSE_OR_POP,
+                  JUMP_IF_TRUE_OR_POP]
 
 double_jump = True if sys.version_info.major == 3 and sys.version_info.minor >= 10 else False
+
 
 def find_first_opcode(co: bytes, op_code: int):
     for i in range(0, len(co), 2):
@@ -33,17 +35,34 @@ def get_arg_bytes(co: bytes, op_code_index: int) -> bytearray:
     This function calculate the argument of a call while considering the EXTENDED_ARG opcodes that may come before that
     """
     result = bytearray()
-    result.append(co[op_code_index+1])
+    result.append(co[op_code_index + 1])
 
     checked_opcode = op_code_index - 2
     while checked_opcode >= 0 and co[checked_opcode] == EXTENDED_ARG:
         result.insert(0, co[checked_opcode + 1])
-        checked_opcode-=2
+        checked_opcode -= 2
     return result
+
 
 def calculate_arg(co: bytes, op_code_index: int) -> int:
     return int.from_bytes(get_arg_bytes(co, op_code_index), 'big')
 
+
+def calculate_extended_args(arg: int): # This function will calculate the necessary extended_args needed
+    extended_args = []
+    new_arg = arg
+    if arg > 255:
+        extended_arg = arg >> 8
+        while True:
+            if extended_arg > 255:
+                extended_arg -= 255
+                extended_args.append(255)
+            else:
+                extended_args.append(extended_arg)
+                break
+
+        new_arg = arg % 256
+    return extended_args, new_arg
 
 def output_code(obj):
     if isinstance(obj, types.CodeType):
@@ -51,12 +70,13 @@ def output_code(obj):
         if "pytransform" in obj.co_freevars:
             #  obj.co_name not in ["<lambda>", 'check_obfuscated_script', 'check_mod_pytransform']:
             pass
-        elif "__armor_enter__" in obj.co_names: 
+        elif "__armor_enter__" in obj.co_names:
             obj = handle_armor_enter(obj)
         else:
             pass
 
     return obj
+
 
 def handle_armor_enter(obj: types.CodeType):
     raw_code = obj.co_code
@@ -64,14 +84,14 @@ def handle_armor_enter(obj: types.CodeType):
     try_start = find_first_opcode(obj.co_code, SETUP_FINALLY)
 
     size = calculate_arg(obj.co_code, try_start)
-    raw_code = raw_code[:try_start+size]
+    raw_code = raw_code[:try_start + size]
 
-    raw_code = raw_code[try_start+2:]
-    raw_code += RETURN_OPCODE # add return # TODO this adds return none to everything? what?
+    raw_code = raw_code[try_start + 2:]
+    raw_code += RETURN_OPCODE  # add return # TODO this adds return none to everything? what?
 
-    load_exit_function = b''.join(i.to_bytes(1, byteorder='big') for i in [LOAD_GLOBAL, obj.co_names.index("__armor_exit__")])
+    load_exit_function = b''.join(
+        i.to_bytes(1, byteorder='big') for i in [LOAD_GLOBAL, obj.co_names.index("__armor_exit__")])
     fake_exit = obj.co_code.find(load_exit_function) - 2
-    
 
     raw_code = bytearray(raw_code)
     i = 0
@@ -83,19 +103,19 @@ def handle_armor_enter(obj: types.CodeType):
             if double_jump: argument *= 2
 
             if argument == fake_exit:
-                raw_code[i] = opcode.opmap["RETURN_VALUE"] # Got to use this because the variable is converted to bytes
+                raw_code[i] = opcode.opmap["RETURN_VALUE"]  # Got to use this because the variable is converted to bytes
                 continue
 
-            new_arg = argument - (try_start+2)
+            new_arg = argument - (try_start + 2)
 
             extended_args, new_arg = calculate_extended_args(new_arg)
 
             for extended_arg in extended_args:
                 raw_code.insert(i, EXTENDED_ARG)
-                raw_code.insert(i+1, extended_arg if not double_jump else extended_arg//2)
+                raw_code.insert(i + 1, extended_arg if not double_jump else extended_arg // 2)
                 i += 2
 
-            raw_code[i+1] = new_arg if not double_jump else new_arg//2
+            raw_code[i + 1] = new_arg if not double_jump else new_arg // 2
 
         i += 2
 
